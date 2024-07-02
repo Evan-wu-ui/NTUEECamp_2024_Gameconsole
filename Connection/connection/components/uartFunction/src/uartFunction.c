d6293b30400452a85a193427d8dd7818e61172fe
Evan-wu-ui <yuliangwu034@gmail.com>
2024-07-03 00:31:18 +0800

    communication accelerate

--- Connection/connection/components/uartFunction/src/uartFunction.c
+++ Connection/connection/components/uartFunction/src/uartFunction.c
@@ -2,12 +2,12 @@
 #include "freertos/FreeRTOS.h"
 #include "driver/uart.h"
 
+#include <string.h>
 #include "uartFunction.h"
 
 #define UART_PORT UART_NUM_2
 #define BUF_SIZE (128)//緩衝區128bytes( 1個loop最多只能收128bytes)
 int timeoutMs=100;
-
 void uartSetup() {
     uart_config_t uart_config = {
         .baud_rate = 921600,
@@ -30,19 +30,17 @@ uint8_t receiveUint8() {
     int len = uart_read_bytes(UART_PORT, &ch, 1, timeoutMs/portTICK_PERIOD_MS);
     if (len > 0) {
         return (uint8_t)ch;//接收uint8
-    } else return 0;//超過timeout
-}
-void sendCharArray(const char* a) {
-    int i = 0;
-    while (a[i] != '\0') {
-        uart_write_bytes(UART_PORT, &a[i], 1);//charArray一項一項傳送
-        i++;
+    } else{
+        return 0;//超過timeout
     }
-    uart_write_bytes(UART_PORT, "\0", 1); // 傳送终止符
+}
+
+void sendCharArray(const char* a) {
+    uart_write_bytes(UART_PORT, a, strlen(a) + 1); // 发送字符串包括终止符
 }
 
 char* receiveCharArray() {
-    char* str = (char*)malloc(BUF_SIZE);//動態陣列設空間為128bytes，暫時的(會釋放)
+    char* str = (char*)malloc(40);//動態陣列設空間為40bytes
     int index = 0;//紀錄接收幾個字元(從0開始)
     char a;
     while (true) {
@@ -55,10 +53,7 @@ char* receiveCharArray() {
             return NULL;//超過timeout
         }
     }
-    char* ans=(char*)malloc(index+1);//ans的記憶體空間=陣列長度+1(截止符)
-    for(int i=0;i<index+1;i++)ans[i]=str[i];//把str移到ans
-    free(str);
-    return ans;//回傳的ans不會有空著但被占用的記憶體
+    return str;
 }
 
 void sendChar(char a) {
@@ -70,7 +65,9 @@ char receiveChar() {
     int len = uart_read_bytes(UART_PORT, &ans, 1, timeoutMs/portTICK_PERIOD_MS);
     if (len > 0) {
         return ans;//接收char
-    } else return '\0';//超過timeout
+    } else {
+        return '\0';//超過timeout
+    }
 }
 void setTimeoutMs(int time){
     timeoutMs=time;//設置timeout(預設100ms)
